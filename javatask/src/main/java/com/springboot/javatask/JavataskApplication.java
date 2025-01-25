34b75eb8b63319db61c0e04466a2257e92f51515
mayurkumbhar27 <mayurshivajikumbhar1999@gmail.com>
2025-01-25 22:54:24 +0530

    Add files via upload

---
+++ javatask/src/main/java/com/springboot/javatask/JavataskApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.javatask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavataskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavataskApplication.class, args);
+	}
+
+}
